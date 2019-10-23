67d4d10ba6e066e783b47fd364d76c428e3af9d3
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2019-10-23 18:10:07 -0300

    #2  - Problema ao renderizar background da página

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,30 +3,30 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load the environment and create the application instance
-| that serves as the central piece of this framework. We'll use this
-| application as an "IoC" container and router for this framework.
-|
-*/
-
-$app = new Laravel\Lumen\Application(
-    dirname(__DIR__)
-);
-
-// $app->withFacades();
-
-// $app->withEloquent();
-
-/*
-|--------------------------------------------------------------------------
+	dirname(__DIR__)
+	))->bootstrap();
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Create The Application
+	|--------------------------------------------------------------------------
+	|
+	| Here we will load the environment and create the application instance
+	| that serves as the central piece of this framework. We'll use this
+	| application as an "IoC" container and router for this framework.
+	|
+	*/
+	
+	$app = new Laravel\Lumen\Application(
+		dirname(__DIR__)
+	);
+	
+	$app->withFacades();
+	
+	$app->withEloquent();
+	
+	/*
+	|--------------------------------------------------------------------------
 | Register Container Bindings
 |--------------------------------------------------------------------------
 |
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,61 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+use App\Models\Quote;
+
+// $router->get('/', function () use ($router) {
+// 	return $router->app->version();
+// });
+
+/**
+ * Display the today quote
+ */
+$router->get('/', function() use ($router) {
+
+    /*
+     * Picks a different quote every day 
+     * (for a maximum of 366 quotes)
+     *
+     *   - $count: the total number of available quotes
+     *   - $day: the current day of the year (from 0 to 365)
+     *   - $page: the page to look for to retrieve the 
+     *            correct record
+     */
+    $count = Quote::query()->get()->count();
+    $day = (int) date('z');
+    $page = $day % $count + 1;
+
+    $quotes = Quote::query()->get()->forPage($page, 1)->all();
+
+    if (empty($quotes)) {
+        throw new \Illuminate\Database\Eloquent\ModelNotFoundException();
+    }
+
+    return view('quote', ['quote' => $quotes[1]]);
 });
+
+/**
+ * Display a specific quote
+ */
+$router->get('/{id}', function($id) use ($router) {
+    $quote = Quote::query()->findOrFail($id);
+    return view('quote', ['quote' => $quote]);
+});
+
+/** 
+ * Testes
+*/
+
+// $router->get('/', function () use ($router) {
+// 	return $router->app->version();
+// });
+
+// $router->get('user/', function (){
+// 	$users = DB::select("SELECT * FROM quotes");
+// 	return $users;
+// });
+
+// $router->get('user/{id}', function ($id){
+// 	$user = DB::select("SELECT * FROM quotes WHERE id = $id");
+// 	return $user;
+// });
